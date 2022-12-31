7beffca857073e274502b143d81f69ad54b4f822
PRIBADI <m.yoga.p30@gmail.com>
2022-12-31 13:31:41 +0700

    Tugas Assertion

---
+++ Section 5/tugasAssertions.cy.js
@@ -0,0 +1,15 @@
+/// <reference types="cypress" />
+    it('clicking "type" shows the right headings' , () => {
+        cy.visit('https://example.cypress.io')
+
+        cy.pause()
+
+        cy.contains('type').click()
+
+        // Should be on a new URL which includes '/commands/actions'
+        cy.url().should('include', '/commands/actions')
+
+      // Get an input, type into it and verify that the value has been updated
+        cy.get('.action-email').type('fake1@email.com').should('have.value', 'fake1@email.com')
+
+    });
