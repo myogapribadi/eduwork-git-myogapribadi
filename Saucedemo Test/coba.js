913e5e887d614f31288f1e54ab3d29d6e4875404
myogapribadi <m.yoga.p30@gmail.com>
2023-01-04 13:51:26 +0700

    TT
    Test file JS

---
+++ Saucedemo Test/coba.js
@@ -0,0 +1,7 @@
+
+    <script>
+        document.getElementById("number").innerHTML = 19999;
+        document.getElementById("number-decimal").innerHTML = 19.999;
+        document.getElementById("string-single").innerHTML = 'Hu Tao';
+        document.getElementById("string-double").innerHTML = "Keqing";
+    </script>
